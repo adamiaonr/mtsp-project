75dbcfcd69fc4078e5c0f575df01671fa4c1f296
adamiaonr <adamiaonr@gmail.com>
2014-07-06 16:50:52 +0100

    Successfully tested with tree topology
    Corrected bug on cache hit and miss graphs.

--- code/simulate.m
+++ code/simulate.m
@@ -277,6 +277,8 @@ subplot(1, 2, 1);
 grid on;
 hold on;
 
+axis([1 (clnt_n + rtr_n + srvr_n) 0 (1.05 * max([max(isent) max(ircvd)]))]);
+
 xlabel('Node index');
 ylabel('# of Interests');
 
@@ -294,6 +296,8 @@ subplot(1, 2, 2);
 grid on;
 hold on;
 
+axis([1 (clnt_n + rtr_n + srvr_n) 0 (1.05 * max([max(dsent) max(drcvd)]))]);
+
 xlabel('Node index');
 ylabel('# of Data packets');
 
@@ -306,34 +310,101 @@ title(sprintf('Data packets'));
 
 hold off;
 
-% 6.2) Cache hit and miss rate per content object, at different NDN router
-% levels (this implies the definition of level, encoded in the rtr_level 
-% matrix)
+% 6.2) same thing as 6.1, but applied to topology levels (not individual
+% clients, routers or servers)
 rtr_level_n = size(rtr_level, 2);
 
+isent = zeros(1, rtr_level_n);
+ircvd = zeros(1, rtr_level_n);
+dsent = zeros(1, rtr_level_n);
+drcvd = zeros(1, rtr_level_n);
+
+for i = 1:rtr_level_n
+
+    level_rtrs = find(rtr_level(:,i));
+
+    for j = 1:numel(level_rtrs)
+
+        isent(i) = isent(i) + sum(sum(nodes(level_rtrs(j)).ifaces.stats_interests_sent, 2));
+        ircvd(i) = ircvd(i) + sum(sum(nodes(level_rtrs(j)).ifaces.stats_interests_rcvd, 2));
+        dsent(i) = dsent(i) + sum(sum(nodes(level_rtrs(j)).ifaces.stats_data_sent, 2));
+        drcvd(i) = drcvd(i) + sum(sum(nodes(level_rtrs(j)).ifaces.stats_data_rcvd, 2));
+        
+    end
+    
+end
+
+figure();
+
+subplot(1, 2, 1);
+
+grid on;
+hold on;
+
+axis([1 (rtr_level_n) 0 (1.05 * max([max(isent) max(ircvd) max(dsent) max(drcvd)]))]);
+
+xlabel('Topology level');
+ylabel('# of Interests');
+
+isent_ = plot (1:1:rtr_level_n, isent,  '-ob', 'LineWidth', 1);
+ircvd_ = plot (1:1:rtr_level_n, ircvd,  '-or', 'LineWidth', 1);
+
+legend([isent_ ircvd_], 'sent', 'received');
+
+title(sprintf('Interests per level'));
+
+hold off;
+
+subplot(1, 2, 2);
+
+grid on;
+hold on;
+
+xlabel('Topology level');
+ylabel('# of Data packets');
+
+dsent_ = plot (1:1:rtr_level_n, dsent,  '-ob', 'LineWidth', 1);
+drcvd_ = plot (1:1:rtr_level_n, drcvd,  '-or', 'LineWidth', 1);
+
+legend([dsent_ drcvd_], 'sent', 'received');
+
+title(sprintf('Data packets per level'));
+
+hold off;
+
+% 6.3) Cache hit and miss rate per content object, at different NDN router
+% levels (this implies the definition of level, encoded in the rtr_level 
+% matrix)
 hit_level = zeros(content_n, rtr_level_n);
 hit_level_reg = zeros(content_n, rtr_level_n);
 miss_level = zeros(content_n, rtr_level_n);
 miss_level_reg = zeros(content_n, rtr_level_n);
 
-% 6.2.1) for each router level, gather statistics and pre-process some
+% 6.3.1) for each router level, gather statistics and pre-process some
 % values
-for i = 2:(rtr_level_n - 1)
 
-    % 6.2.1.1) find out which routers belong to level i
+for i = 2:(rtr_level_n - 1)
+    
+    icumul = zeros(content_n, 1);
+
+    % 6.3.1.1) find out which routers belong to level i
     level_rtrs = find(rtr_level(:,i));
 
-    % 6.2.1.2) simply gather the statistics for level i
+    % 6.3.1.2) simply gather the statistics for level i
     for j = 1:numel(level_rtrs)
 
-        hit_level(:,i) = hit_level(:,i) + nodes(level_rtrs(j)).CS.stats_hits ./ (nodes(level_rtrs(j)).ifaces.stats_interests_rcvd(:, 1) + 1);    
-        miss_level(:,i) = miss_level(:,i) + nodes(level_rtrs(j)).CS.stats_miss ./ (nodes(level_rtrs(j)).ifaces.stats_interests_rcvd(:, 1) + 1);
+        hit_level(:,i) = hit_level(:,i) + (nodes(level_rtrs(j)).CS.stats_hits);    
+        miss_level(:,i) = miss_level(:,i) + (nodes(level_rtrs(j)).CS.stats_miss);
+        icumul = icumul + nodes(level_rtrs(j)).ifaces.stats_interests_rcvd(:, 1) + 1;
 
     end
-    
-    % 6.2.1.3) in order to get a well defined line and not simply a bunch of
+        
+    % 6.3.1.3) in order to get a well defined line and not simply a bunch of
     % points scattered all over the place, apply linear regression to find
     % it
+    hit_level(:,i) = hit_level(:,i) ./ icumul;
+    miss_level(:,i) = miss_level(:,i) ./ icumul;
+    
     hit_level_reg(:,i) = poly_regression((1:1:content_n)', hit_level(:,i), (1:1:content_n)', 3);
     miss_level_reg(:,i) = poly_regression((1:1:content_n)', miss_level(:,i), (1:1:content_n)', 3);
 end
@@ -348,7 +419,7 @@ end
 %     
 % end
 
-% 6.2.2) after the statistics gathering and pre-processing, the actual
+% 6.3.2) after the statistics gathering and pre-processing, the actual
 % plots, one subplot for the cache hits, another for cache misses
 
 figure();
@@ -358,13 +429,13 @@ subplot(1, 2, 1);
 grid on;
 hold on;
 
-% 6.2.2.1) apparently, this is important
+% 6.3.2.1) apparently, this is important
 axis([0 content_n 0 1]);
 
 xlabel('Content index');
 ylabel('Hit rate');
 
-% 6.2.2.2) some references for the dynamic plot list (both for the plot
+% 6.3.2.2) some references for the dynamic plot list (both for the plot
 % objects and the legend strings)
 plot_obj = zeros(1, rtr_level_n);
 plot_str = cell(1, rtr_level_n);
@@ -385,14 +456,14 @@ end
 %     
 % end
 
-% 6.2.2.3) and this is how you build up a dynamic legend...
+% 6.3.2.3) and this is how you build up a dynamic legend...
 legend(plot_obj(2:(rtr_level_n - 1)), plot_str{2:(rtr_level_n - 1)});
 
 title(sprintf('Cache hit rate'));
 
 hold off;
 
-% 6.2.2.4) do the sames as above, now for cache misses
+% 6.3.2.4) do the sames as above, now for cache misses
 subplot(1, 2, 2);
 
 grid on;
@@ -426,8 +497,8 @@ title(sprintf('Cache miss rate'));
 
 hold off;
 
-% 6.3) Caching time per content object, at different NDN router
-% levels (basically, the same procedures as those applied in 6.2 are used
+% 6.4) Caching time per content object, at different NDN router
+% levels (basically, the same procedures as those applied in 6.3 are used
 % here)
 time_rtr_level = zeros(content_n, rtr_level_n);
 
@@ -465,65 +536,6 @@ end
 legend(plot_obj(2:(rtr_level_n - 1)), plot_str{2:(rtr_level_n - 1)});
 
 title(sprintf('Relative time in CS'));
-
-hold off;
-
-% 6.4) same thing as 6.1, but applied to topology levels (not individual
-% clients, routers or servers)
-
-isent = zeros(1, rtr_level_n);
-ircvd = zeros(1, rtr_level_n);
-dsent = zeros(1, rtr_level_n);
-drcvd = zeros(1, rtr_level_n);
-
-for i = 1:rtr_level_n
-
-    level_rtrs = find(rtr_level(:,i));
-
-    for j = 1:numel(level_rtrs)
-
-        isent(i) = isent(i) + sum(sum(nodes(level_rtrs(j)).ifaces.stats_interests_sent, 2));
-        ircvd(i) = ircvd(i) + sum(sum(nodes(level_rtrs(j)).ifaces.stats_interests_rcvd, 2));
-        dsent(i) = dsent(i) + sum(sum(nodes(level_rtrs(j)).ifaces.stats_data_sent, 2));
-        drcvd(i) = drcvd(i) + sum(sum(nodes(level_rtrs(j)).ifaces.stats_data_rcvd, 2));
-        
-    end
-    
-end
-
-figure();
-
-subplot(1, 2, 1);
-
-grid on;
-hold on;
-
-xlabel('Topology level');
-ylabel('# of Interests');
-
-isent_ = plot (1:1:rtr_level_n, isent,  '-ob', 'LineWidth', 1);
-ircvd_ = plot (1:1:rtr_level_n, ircvd,  '-or', 'LineWidth', 1);
-
-legend([isent_ ircvd_], 'sent', 'received');
-
-title(sprintf('Interests per level'));
-
-hold off;
-
-subplot(1, 2, 2);
-
-grid on;
-hold on;
-
-xlabel('Topology level');
-ylabel('# of Data packets');
-
-dsent_ = plot (1:1:rtr_level_n, dsent,  '-ob', 'LineWidth', 1);
-drcvd_ = plot (1:1:rtr_level_n, drcvd,  '-or', 'LineWidth', 1);
-
-legend([dsent_ drcvd_], 'sent', 'received');
-
-title(sprintf('Data packets per level'));
 
 hold off;
 
--- code/test.m
+++ code/test.m
@@ -1,18 +1,18 @@
 addpath('~/Workbench/mtsp-project/code/utils/')
 
-%% 1) overall simulation parameters
+%% 1.1) overall simulation parameters (cascade topology)
 
-% 1.1) different number of content objects
+% 1.1.1) different number of content objects
 content_n = 100;
 
-% 1.2) CS size (i.e. number of slots)
+% 1.1.2) CS size (i.e. number of slots)
 cs_size = 25;
 
-% 1.3) number of simulation rounds (i.e. 'generate signals' -> 'fetch
+% 1.1.3) number of simulation rounds (i.e. 'generate signals' -> 'fetch
 % inputs' -> process inputs -> set outputs cycles)
 round_n = 1000;
 
-% 1.4) specify the topology matrix (commented case is a cascade 
+% 1.1.4) specify the topology matrix (commented case is a cascade 
 % topology, 1 client, 3 levels - i.e. NDN routers - and 1 server).
 clnt_n = 1;
 rtr_n = 3;
@@ -20,21 +20,82 @@ srvr_n = 1;
 
 topology = [0 1 0 0 0; 1 0 2 0 0; 0 1 0 2 0; 0 0 1 0 2; 0 0 0 1 0];
 
-% 1.5) the router level matrix, i.e. the distribution of routers over the
+% 1.1.5) the router level matrix, i.e. the distribution of routers over the
 % topology levels, codified in a nodes x rtr_level_n matrix
 rtr_level_n = 3;
 
-% 1.5.1) in this case, 1 router per level
+% 1.1.5.1) in this case, 1 router per level
 rtr_level = [1 0 0 0 0; 0 1 0 0 0; 0 0 1 0 0; 0 0 0 1 0; 0 0 0 0 1];
 
-% 1.6) content popularity, a zipf distribution, in the style of Carofiglio
+% 1.1.6) content popularity, a zipf distribution, in the style of Carofiglio
 % et al. 2011, p = c / (k^(alpha))
 c = 0.8;
 alpha = 1.0;
 k = 1:1:content_n;
 content_popularity = c ./ (k.^(alpha));
 
-%% 2) simulation rounds
+%% 1.2) simulation rounds
+% simulate(content_n, content_popularity, cs_size, 'LRU', round_n, topology, clnt_n, rtr_n, rtr_level, srvr_n);
+% simulate(content_n, content_popularity, cs_size, 'RANDOM', round_n, topology, clnt_n, rtr_n, rtr_level, srvr_n);
+% simulate(content_n, content_popularity, cs_size, 'MRU', round_n, topology, clnt_n, rtr_n, rtr_level, srvr_n);
+
+%% 2.1) tree topology
+
+% 2.1.1) specify the topology matrix
+clnt_n = 8;
+rtr_n = 7;
+srvr_n = 1;
+
+topology = [
+    % clnts
+% column numbers in HEX
+%   1 2 3 4 5 6 7 8 9 A B C D E F 10
+    0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0; 
+    0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0; 
+    0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0; 
+    0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0; 
+    0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0; 
+    0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0; 
+    0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0; 
+    0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0; 
+    % rtrs
+%   1 2 3 4 5 6 7 8 9 A B C D E F 10
+    1 1 0 0 0 0 0 0 0 0 0 0 2 0 0 0; 
+    0 0 1 1 0 0 0 0 0 0 0 0 2 0 0 0; 
+    0 0 0 0 1 1 0 0 0 0 0 0 0 2 0 0; 
+    0 0 0 0 0 0 1 1 0 0 0 0 0 2 0 0; 
+    0 0 0 0 0 0 0 0 1 1 0 0 0 0 2 0; 
+    0 0 0 0 0 0 0 0 0 0 1 1 0 0 2 0; 
+    0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 2; 
+    % srvr
+%   1 2 3 4 5 6 7 8 9 A B C D E F 10
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0];
+
+% 2.1.2) the router level matrix, i.e. the distribution of routers over the
+% topology levels, codified in a nodes x rtr_level_n matrix
+rtr_level_n = 5;
+
+% 2.1.2.1) in this case, 1 router per level
+rtr_level = [
+    1 0 0 0 0; 
+    1 0 0 0 0; 
+    1 0 0 0 0; 
+    1 0 0 0 0;
+    1 0 0 0 0; 
+    1 0 0 0 0; 
+    1 0 0 0 0; 
+    1 0 0 0 0;
+    0 1 0 0 0; 
+    0 1 0 0 0; 
+    0 1 0 0 0; 
+    0 1 0 0 0;
+    0 0 1 0 0; 
+    0 0 1 0 0; 
+    0 0 0 1 0; 
+    0 0 0 0 1;
+    ];
+
+%% 2.2) simulation rounds
 simulate(content_n, content_popularity, cs_size, 'LRU', round_n, topology, clnt_n, rtr_n, rtr_level, srvr_n);
-simulate(content_n, content_popularity, cs_size, 'RANDOM', round_n, topology, clnt_n, rtr_n, rtr_level, srvr_n);
-simulate(content_n, content_popularity, cs_size, 'MRU', round_n, topology, clnt_n, rtr_n, rtr_level, srvr_n);
+%simulate(content_n, content_popularity, cs_size, 'RANDOM', round_n, topology, clnt_n, rtr_n, rtr_level, srvr_n);
+%simulate(content_n, content_popularity, cs_size, 'MRU', round_n, topology, clnt_n, rtr_n, rtr_level, srvr_n);
