6f13447a62bbde9534c7ad7995ea3d6a8276cc36
adamiaonr <adamiaonr@gmail.com>
2014-07-10 11:52:01 +0100

    Now plotting popularity distribution (log scale).
    Also plotting in black and white printer friendly manner (should extend
    this to other plots).
    Still no 2:1 ratio on plots and subplots.

--- code/test.m
+++ code/test.m
@@ -11,11 +11,11 @@ content_n = 100;
 
 % 1.1.2) CS size (i.e. number of slots)
 %cs_size = [10 25 50 75];
-cs_size = [50];
+cs_size = [25];
 
 % 1.1.3) number of simulation rounds (i.e. 'generate signals' -> 'fetch
 % inputs' -> process inputs -> set outputs cycles)
-round_n = 1000;
+round_n = 10000;
 
 % 1.1.4) specify the topology matrix (commented case is a cascade 
 % topology, 1 client, 3 levels - i.e. NDN routers - and 1 server).
@@ -34,15 +34,52 @@ rtr_level = [1 0 0 0 0; 0 1 0 0 0; 0 0 1 0 0; 0 0 0 1 0; 0 0 0 0 1];
 % 1.1.6) content popularity, a zipf distribution, in the style of Carofiglio
 % et al. 2011, p = c / (k^(alpha))
 c = 0.8;
-alpha = 1.0;
+alpha = [0.25, 0.5, 1.0, 2.0];
 k = 1:1:content_n;
-content_popularity = c ./ (k.^(alpha));
+
+content_popularity = zeros(numel(alpha), content_n);
+
+% 1.1.7) plot content popularity distributions
+
+figure();
+
+grid on;
+hold on;
+
+axis([1 content_n 0 1]);
+axis square;
+
+xlabel('Content index');
+ylabel('Popularity');
+
+markers = {'-ko', '-ks', '-k^', '--ko', '--ks', '--k^'};
+
+plot_obj = zeros(1, numel(alpha));
+plot_str = cell(1, numel(alpha));
+
+for l = 1:numel(alpha)
+    
+    content_popularity(l,:) = (c ./ (k.^(alpha(l))));
+        
+    plot_obj(l) = semilogx(1:1:content_n, content_popularity(l,:), sprintf('%s', markers{l}), 'LineWidth', 1);
+    plot_str{l} = sprintf('alpha = %0.2f', alpha(l));    
+    
+    % 1.1.8) this is necessary, according to 
+    % http://www.mathworks.com/matlabcentral/newsreader/view_thread/35174
+    set(gca,'xscale','log');
+end
+
+legend(plot_obj(1:numel(alpha)), plot_str{1:numel(alpha)});
+
+title(sprintf('Content popularity'));
+
+hold off;
 
 %% 1.2) simulation rounds
 
 for j = 1:numel(cs_size)
 
-    simulate(content_n, content_popularity, cs_size(j), 'LRU', round_n, topology, clnt_n, rtr_n, rtr_level, srvr_n, figure_dir_cascade);
+    simulate(content_n, content_popularity(4,:), cs_size(j), 'LRU', round_n, topology, clnt_n, rtr_n, rtr_level, srvr_n, figure_dir_cascade);
     %simulate(content_n, content_popularity, cs_size(j), 'MRU', round_n, topology, clnt_n, rtr_n, rtr_level, srvr_n, figure_dir_cascade);
     %simulate(content_n, content_popularity, cs_size(j), 'RANDOM', round_n, topology, clnt_n, rtr_n, rtr_level, srvr_n, figure_dir_cascade);
     
